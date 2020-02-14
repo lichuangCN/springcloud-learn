8e0d5a2dfedd6acc8f6689245fdc56f0ed667455
lichuangCN <1503031032@qq.com>
2020-02-14 22:20:55 +0800

    完成Eureka注册模块

---
+++ eureka-client/src/main/java/site/muzhi/springcloud/EurekaClientApplication.java
@@ -0,0 +1,18 @@
+package site.muzhi.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * 注册为Eureka客户端
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+}
---
+++ eureka-security-server/src/test/java/site/muzhi/springcloud/EurekaSecurityServerApplicationTests.java
@@ -0,0 +1,13 @@
+package site.muzhi.springcloud;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaSecurityServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
